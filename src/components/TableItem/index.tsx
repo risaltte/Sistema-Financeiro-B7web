d90ae801404ca90a19264c7c9f74538b6b7a2a72
Rafael Risalte <risaltte@gmail.com>
2022-02-02 22:26:35 -0400

    TableItem correction

--- src/components/TableItem/index.tsx
+++ src/components/TableItem/index.tsx
@@ -12,13 +12,17 @@ export const TableItem = ({ item }: Props) => {
     return (
         <C.TableLine>
             <C.TableColumn>{formatDate(item.date)}</C.TableColumn>
+            <C.TableColumn>
                 <C.Category color={categories[item.category].color}>
-                    <C.TableColumn>{categories[item.category].title}</C.TableColumn>                
+                    {categories[item.category].title}               
                 </C.Category>
-                <C.TableColumn>{item.title}</C.TableColumn>
+            </C.TableColumn>                
+            <C.TableColumn>{item.title}</C.TableColumn>
+            <C.TableColumn>
                 <C.Value color={categories[item.category].expense ? 'red' : 'green'}>
-                    <C.TableColumn >{formatMoneyBr(item.value)}</C.TableColumn>
+                    {formatMoneyBr(item.value)}
                 </C.Value>
+            </C.TableColumn>
         </C.TableLine>
     );
 };
