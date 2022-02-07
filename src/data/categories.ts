194ef352855a8af1002130ccd9acf897f51ee7e7
Rafael Risalte <risaltte@gmail.com>
2022-02-06 22:02:50 -0400

    Adjustments in categories and items data

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,7 +1,14 @@
 import { Category } from "../types/Category"
 
 export const categories: Category = {
-    food: {title: 'Alimentação', color: 'blue', expense: true},
-    rent: {title: 'Aluguel', color: 'brown', expense: true},
-    salary: {title: 'Salário', color: 'green', expense: false},
+    food: {title: 'Alimentação', color: '#1C05FC', expense: true},
+    rent: {title: 'Aluguel', color: '#FF5147', expense: true},
+    car: {title: 'Carro', color: '#F58C31', expense: true},    
+    house: {title: 'Casa', color: '#FC056D', expense: true},
+    Leisure: {title: 'Lazer', color: '#8A6D00', expense: true},        
+    extraIncome: {title: 'Extra', color: '#038F0D', expense: true},        
+    clothes: {title: 'Roupas', color: '#332011', expense: true}, 
+    salary: {title: 'Salário', color: '#4CF507', expense: false},
+    supermarket: {title: 'Supermercado', color: '#F5D232', expense: true},    
+    others: {title: 'Outros', color: '#CB11E6', expense: true},    
 };
--- src/data/items.ts
+++ src/data/items.ts
@@ -1,8 +1,6 @@
 import { Item } from "../types/Item";
 
 export const items: Item[] = [
-    { id: 'abcd', date: new Date(2022, 0, 5 ), category: 'food', title: 'McDonalds', value: 32.12 },
-    { id: 'efgh', date: new Date(2022, 0, 15 ), category: 'food', title: 'Burger King', value: 28 },
-    { id: 'ijkl', date: new Date(2022, 0, 20 ), category: 'rent', title: 'Aluguel Apt', value: 2300 },
-    { id: 'mnop', date: new Date(2022, 1, 12 ), category: 'salary', title: 'Salário ACME', value: 4500 },
+    { id: 'mnop', date: new Date(), category: 'salary', title: 'Salário ACME', value: 4500 },
+    { id: 'ijkl', date: new Date(), category: 'rent', title: 'Aluguel Apt', value: 2300 },
 ];
